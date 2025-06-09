8bd01dc5d12219e69c29ea2908a16dd25b539f32
505Brewcrew <kody.martin35@gmail.com>
2025-06-09 16:29:59 -0600

    Update app.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,10 +1,10 @@
 /*jshint esversion: 6 */
 
+import Profile from './components/Profile/Profile';
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
-
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
@@ -28,13 +28,14 @@ const authRoutes = require('./routes/authRoutes');
 const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
-
+<Route path="/app/profile" element={<Profile/>} />
 app.use(pinoHttp({ logger }));
 
 // Use Routes
 app.use('/api/gifts', giftRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/search', searchRoutes);
+
 
 // Global Error Handler
 app.use((err, req, res, next) => {
